030963aafabbe6aaa1731ab460214b853a8d3885
CongPp1 <103237012+CongPp1@users.noreply.github.com>
2024-10-02 17:50:53 +0700

    Set up React_Vite

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Button } from './components/ui/button'
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen grid place-items-center">
+      <Button variant="default" size="default">Button</Button>
+    </div>
+  )
+}
